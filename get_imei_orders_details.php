da7fac4dd925b103b3152aae45f869447a0b61c5
Itay Mishan <mishan.itay@gmail.com>
2016-05-08 10:11:18 -0400

    Update get_imei_orders_details.php

--- get_imei_orders_details.php
+++ get_imei_orders_details.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
-
- *	@author Dhru.com
-
- *	@APi kit version 2.0 March 01, 2012
-
- *	@Copyleft GPL 2001-2011, Dhru.com
-
- **/
  
 require ('header.php');
 include ('dhrufusionapi.class.php');
